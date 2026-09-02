2e7fe6e5ef42ee0c44d3c49b6f868e52d2ad94be
Isis Vargas <66917675+the-pale-blue-pixel@users.noreply.github.com>
2026-09-02 18:20:32 -0400

    Update formulario-taller.js

--- formulario-taller.js
+++ formulario-taller.js
@@ -1,5 +1,5 @@
 // Pega aquí la URL terminada en /exec que obtendrás al publicar el Apps Script.
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzIWKzqnjhCHdgBec_fRsKaT_Spkqew5B0hqJY-6pmp3bTkMvP4K9zttNgpHWuOMxY/exec";
+const GOOGLE_SCRIPT_URL = const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz_Q8uYWcFbG6Z7rIb3S3G1plBpUZxNAB-QLSe8_aHRLt0w7NBflcneyQZJMPQ1c2Msvg/exec";
 
 const form = document.getElementById("taller-form");
 const submitButton = document.getElementById("submit-button");
